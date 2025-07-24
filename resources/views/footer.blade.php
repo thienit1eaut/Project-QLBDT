16ad1a03149a6fc2edac9c0eef610e2acc650b27
thieneautit1 <20220062@eaut.edu.vn>
2025-07-24 18:06:15 +0700

    Delete resources directory

--- resources/views/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="footer mt-auto py-3">
-    <div class="container-fluid">
-        <span class="text-muted">Copyright © 2025 | Powered by <a href="https://laravelcrm.com" target="_blank" rel="noopener noreferrer">Laravel CRM</a></span>
-    </div>
-</footer>
